741c572b11b395be8a84ad15f8b7fb64d101631f
Huzaifa Arshad <huzaifahussain627@gmail.com>
2025-03-08 14:48:16 +0500

    fix sellerlogin error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as Login } from "./Login/Login";
-export { default as SellerLogin } from "./SellerLogin/Sellerlogin";
+// export { default as SellerLogin } from "./SellerLogin/Sellerlogin";
 export { default as Signup } from "./Signup/Signup";
 export { default as SellerSignup } from "./SellerSignup/SellerSignup";
 export { default as Forgotpassword } from "./Forgotpassword/Forgotpassword";
